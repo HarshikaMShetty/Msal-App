8217d974989013e2693889a7d792a773eb1d81bf
HarshikaMShetty <89129865+HarshikaMShetty@users.noreply.github.com>
2022-11-25 10:46:12 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,20 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-      "source": "/:path*",
-      "has": [
-        {
-          "type": "host",
-          "value": "private.yamunamathiasheights.com.com"
-        }
-      ],
-      "destination": "/private/:path*"
-    }
-    ]
-  },
 }
 
 module.exports = nextConfig
